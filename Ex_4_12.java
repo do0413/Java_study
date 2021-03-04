35b2e5d368413daeb256860e8245ab3f74a7de9c
do0413 <wawa4989@naver.com>
2021-03-04 16:00:42 +0900

    2021-03-04 study

---
+++ Ex_4_12.java
@@ -0,0 +1,40 @@
+package PDF_jeongSeok;
+
+public class Ex_4_12 {
+
+	public static void main(String[] args) {
+		// 구구단의 일부를 출력하시오.
+		int front=-1;
+		int back=0;
+		int num1=0;
+
+		for (int i=1; i<=3; i++) {
+			front=-1;
+			front += 3*i;
+			
+			
+			
+			for (int j=1; j<=3; j++) {
+				num1= front;
+				back++;
+				for (int k=1; k<=3; k++) {
+					System.out.print(num1 + "*" + back +"=" + (num1*back) + "    ");
+					num1++;
+					
+					if(num1 >=10) {
+						break;
+					}
+				}
+				System.out.println("");
+			
+				
+			}
+			System.out.println("");
+			
+			
+			
+		}//for
+		
+	}//main
+
+}
